2cae583f6eb0fd281a20ff956aa002cb2def334f
pablohdez06 <pablohs.granada@gmail.com>
2026-03-12 20:23:59 +0100

    Organicación de los proyectos

--- postgres/init.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS conversaciones_chatbot (
-    id SERIAL PRIMARY KEY,
-    chatID VARCHAR(255),
-    username VARCHAR(255),
-    mensajeUser TEXT,
-    mensajeIA TEXT,
-    fecha TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
